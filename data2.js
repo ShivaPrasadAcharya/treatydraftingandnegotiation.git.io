67f0b51d4e884806d7969695877cd7b78323b37a
ShivaPrasadAcharya <shivaprasadacharya000000000000@gmail.com>
2025-06-29 23:30:41 +0545

    Add files via upload

---
+++ data2.js
@@ -0,0 +1,15 @@
+// data2.js - Images Dataset
+var data2 = `ID,Title,Description,Image
+001,Handbook on Good Treaty Practice,Jill Barret/Robert Beckham,Handbook.jpg
+002,Modern Treaty Law and Practice,Anthony Aust,Moderntreatylaw.jpg
+003, Negotiation,Roy J. Lewicki,Negotiation.jpg
+004, Oxford Guide to Treaties,2nd edition,Oxfordguide.jpg`;
+
+// Dataset metadata
+var data2Info = {
+    name: "Images",
+    description: "Image Gallery",
+    emoji: "🖼️",
+    columns: 4,
+    primaryKey: "ID"
+};
---
+++ data3.js
@@ -0,0 +1,14 @@
+// data3.js - Links Dataset
+var data3 = `ID,Book,Remark,Link
+001, Treaty Schedule(Asar 8-18 2082),word document,document.docx
+002, Treaty Schedule(Asar 8-18 2082),pdf document,document.pdf
+003,Contact Directory, Call & Add trainees, https://shivaprasadacharya.github.io/calltreatytrainees.git.io/ 
+004,Treaty Handbook,prepared by UN (2018),TreatyHandbook.pdf`;
+// Dataset metadata
+var data3Info = {
+    name: "Links",
+    description: "Schedule & Docs Mgmt",
+    emoji: "🔗",
+    columns: 4,
+    primaryKey: "ID"
+};
