cef7d00b92ea38263eaf906d5bd313b6d9cee3e1
ShivaPrasadAcharya <shivaprasadacharya000000000000@gmail.com>
2025-06-30 21:49:46 +0545

    updateddata3withsimulationexercise

--- data3.js
+++ data3.js
@@ -1,9 +1,12 @@
 // data3.js - Links Dataset
 var data3 = `ID,Book,Remark,Link
-001, Treaty Schedule(Asar 8-18 2082),word document,document.docx
-002, Treaty Schedule(Asar 8-18 2082),pdf document,document.pdf
+001, Training Schedule(Asar 8-18 2082),word document,document.docx
+002, Training Schedule(Asar 8-18 2082),pdf document,document.pdf
 003,Contact Directory, Call & Add trainees, https://shivaprasadacharya.github.io/calltreatytrainees.git.io/ 
-004,Treaty Handbook,prepared by UN (2018),TreatyHandbook.pdf`;
+004,Treaty Handbook,prepared by UN (2018),TreatyHandbook.pdf
+005,Simulation Negotiation, Exercise(Asar 18),SimulationNegotiation.docx 
+006,Simulation Negotiation, Exercise(Asar 18),SimulationNegotiation.pdf
+007,Simulation Negotiation, Exercise(Asar 18),SimulationNegotiation.html `;
 // Dataset metadata
 var data3Info = {
     name: "Links",
